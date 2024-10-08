20eace56b29c255e78a00b47ac7964f9334b32be
wangcp <reborn2002@foxmail.com>
2024-10-08 10:52:38 +0800

    submit day08 and day09

---
+++ day08/stSlider.py
@@ -0,0 +1,40 @@
+import streamlit as st
+from datetime import time, datetime
+
+st.header('st.slider')
+
+# 样例 1
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm", age, "years old")
+
+# 样例 2
+
+st.subheader('Range slider')
+
+values = st.slider(
+    'Select a range of values',
+    0.0, 100.0, (25.0, 75.0)
+)
+st.write('Values:', values)
+
+# 样例 3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+    "Schedule your appointment:",
+    value=(time(11, 30), time(12, 45))
+)
+st.write("You're scheduled for:", appointment)
+
+# 样例 4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="YY/MM/DD - hh:mm"
+)
+st.write("Start time:", start_time)
