b4a61367ff36588b65f4015e637bbd2f4b5305e2
wangcp <reborn2002@foxmail.com>
2024-10-08 10:51:03 +0800

    submit day08 and day09

---
+++ day09/stLineChart.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.header('Line chart')
+
+chart_data = pd.DataFrame(
+    np.random.randn(20, 3),
+    columns=['a', 'b', 'c']
+)
+
+st.line_chart(chart_data)
+st.write(chart_data)
