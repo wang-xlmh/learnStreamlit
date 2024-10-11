2adacc3d1b9e354a2b723c65670ed2810fcf0b5f
wangcp <reborn2002@foxmail.com>
2024-10-11 10:48:16 +0800

    create test01 alter HelloWorld

--- test01.py
+++ test01.py
@@ -3,7 +3,7 @@ import base64
 
 
 # 使用缓存装饰器来避免重复加载图片
-@st.cache_data
+@st.cache_data(ttl="1d")
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -12,17 +12,18 @@ def get_base64_of_bin_file(bin_file):
 
 def set_jpg_as_page_bg(jpg_file):
     bin_str = get_base64_of_bin_file(jpg_file)
-    page_bg_img = '''
+    page_bg_img = f'''
     <style>
-    body { 
-    background-image: url("data:image/jpeg;base64,%s");
-    background-size: cover;
-    }
+    .stApp {{
+        background-image: url("data:image/jpeg;base64,{bin_str}");
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+    }}
     </style>
-    ''' % bin_str
+    '''
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
-    return
 
 
 # 假设 'background.jpg' 是与你的 Streamlit 脚本在同一目录下的图片文件
@@ -30,3 +31,4 @@ set_jpg_as_page_bg('background.jpg')
 
 # 添加其他 Streamlit 代码来创建你的应用程序
 st.title('My Streamlit App with Background Image')
+st.write('This is the main content of the app.')
